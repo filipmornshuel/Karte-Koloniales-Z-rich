7e547b28b855154175fd9db39420744a90452219
Filip Slavkovic <slavkovicf@bzz.ch>
2023-03-27 21:37:26 +0200

    initial commit

---
+++ public/js/map.js
@@ -0,0 +1,282 @@
+var addCheckpointMode = false;
+var name;
+var description;
+var coordinate;
+var blobImg;
+var blobAudio;
+const btn = document.getElementById('send');
+function openForm() {
+  document.getElementById('myForm').style.display = 'block';
+  addCheckpointMode = true;
+}
+
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+}
+var vectorSource = new ol.source.Vector();
+
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource,
+  style: function (feature) {
+    var name = feature.get('name');
+    var textStyle = new ol.style.Text({
+      textAlign: 'center',
+      textBaseline: 'middle',
+      font: '12px Arial',
+      text: name,
+      fill: new ol.style.Fill({ color: 'black' }),
+      stroke: new ol.style.Stroke({ color: 'white', width: 3 }),
+      offsetX: 0,
+      offsetY: 15,
+      rotation: 0,
+    });
+
+    var iconStyle = new ol.style.Icon({
+      anchor: [0.5, 1],
+      //blockiert in firefox wegen CORP
+      src: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png',
+      scale: 0.1,
+    });
+
+    return new ol.style.Style({
+      text: textStyle,
+      image: iconStyle,
+    });
+  },
+});
+
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+    vectorLayer,
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([8.5432, 47.369]),
+    zoom: 12,
+  }),
+});
+
+function switchMode() {
+  var modeButton = document.getElementById('modeButton');
+  if (addCheckpointMode) {
+    addCheckpointMode = false;
+    modeButton.innerHTML = 'Switch to Add Checkpoint Mode';
+  } else {
+    addCheckpointMode = true;
+    modeButton.innerHTML = 'Switch to Exploration Mode';
+  }
+}
+
+function saveCheckpointData() {
+  name = document.getElementById('nameMorn').value;
+  description = document.getElementById('description').value;
+  blobImg = document.getElementById('img').value;
+  blobAudio = document.getElementById('audio').value;
+  console.log(name, description, blobImg, blobAudio);
+  addCheckpoint(coordinate, name, description, blobImg);
+  document.getElementById('nameMorn').value = '';
+  document.getElementById('description').value = '';
+  document.getElementById('img').value = '';
+  document.getElementById('audio').value = '';
+  closeForm();
+}
+
+function addCheckpoint(coordinate, name, description, blobImg) {
+  console.log(coordinate, name, description, blobImg);
+  addCheckpointMode = false;
+  if (name != null && name != '') {
+    var checkpoint = new ol.Feature({
+      geometry: new ol.geom.Point(coordinate),
+      name: name,
+      description: description,
+      img: blobImg,
+    });
+    vectorSource.addFeature(checkpoint);
+
+    // send checkpoint data to server
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/checkpoints');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        console.log('Checkpoint saved to database');
+      } else {
+        console.log('Error saving checkpoint to database');
+      }
+    };
+    xhr.send(
+      JSON.stringify({
+        name: name,
+        lng: coordinate[0],
+        lat: coordinate[1],
+        description: description,
+        img: blobImg,
+      })
+    );
+  }
+}
+
+// Diese Funktion sollte noch überarbeitet werden, wenn Zeit übrig bleibt,
+// dass man denn Checkpoint noch variabel verschieben kann, bevor man den Vorschlag gemacht hat.
+function fakeAddCheckpoint(coordinate, name) {
+  console.log(coordinate, name, description, blobImg);
+  addCheckpointMode = false;
+  if (name != null && name != '') {
+    var checkpoint = new ol.Feature({
+      geometry: new ol.geom.Point(coordinate),
+      name: name,
+      description: description,
+      img: blobImg,
+    });
+    vectorSource.addFeature(checkpoint);
+  }
+}
+
+map.on('click', function (evt) {
+  if (addCheckpointMode) {
+    coordinate = evt.coordinate;
+    //alert(coordinate)
+    document.getElementById('cords').value = coordinate;
+    //addCheckpoint(coordinate,name,description)
+    //fakeAddCheckpoint(coordinate,name);
+  }
+});
+
+var checkpoints = [
+  new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.5448, 47.3769])),
+    name: 'Checkpoint 1',
+  }),
+  new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.5219, 47.3721])),
+    name: 'Checkpoint 2',
+  }),
+  new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.5394, 47.391])),
+    name: 'Checkpoint 3',
+  }),
+  new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([8.5188, 47.3613])),
+    name: 'Checkpoint 4',
+  }),
+];
+
+vectorSource.addFeatures(checkpoints);
+
+map.on('singleclick', function (evt) {
+  if (!addCheckpointMode) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+      return feature;
+    });
+
+    if (feature && feature.getProperties().name.startsWith('Checkpoint')) {
+      window.open(
+        'https://en.wikipedia.org/wiki/Zurich#' +
+          feature.getProperties().name.replace(' ', '_'),
+        '_blank'
+      );
+    }
+  }
+});
+
+// evtl. in eine Methode einbauen
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/api/checkpoints', true);
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    let checkpoints = JSON.parse(xhr.responseText);
+    let entries = document.getElementById('checkpoint-entries');
+    let entriesTable = document.createElement('table');
+    const trh = entriesTable.insertRow();
+
+    let th1 = document.createElement('th');
+    th1.innerHTML = 'Name';
+    trh.appendChild(th1);
+
+    let th2 = document.createElement('th');
+    th2.innerHTML = 'Beschreibung';
+    trh.appendChild(th2);
+
+    let th3 = document.createElement('th');
+    th3.innerHTML = 'Bild';
+    trh.appendChild(th3);
+
+    let th4 = document.createElement('th');
+    th4.innerHTML = 'Audio';
+    trh.appendChild(th4);
+
+    console.log(checkpoints);
+
+    for (let i = 0; i < checkpoints.length; i++) {
+      const checkpoint = checkpoints[i];
+      const entry = {
+        name: checkpoint.name,
+        description: checkpoint.description,
+        img: checkpoint.img,
+        audio: checkpoint.audio,
+      };
+
+      const trd = entriesTable.insertRow();
+      let nameCell = trd.insertCell(0);
+      let descCell = trd.insertCell(1);
+      let imgCell = trd.insertCell(2);
+      let audioCell = trd.insertCell(3);
+
+      nameCell.innerHTML = entry.name;
+      descCell.innerHTML = entry.description;
+      imgCell.innerHTML = entry.img;
+      audioCell.innerHTML = entry.audio;
+
+      // Ein neues ol.Feature-Objekt erstellen
+      const feature = new ol.Feature({
+        geometry: new ol.geom.Point([checkpoint.lng, checkpoint.lat]),
+        name: checkpoint.name,
+      });
+      // Füge das Feature zum Vektorlayer hinzu
+      vectorSource.addFeature(feature);
+    }
+    entries.appendChild(entriesTable);
+  } else {
+    console.log('no data has been found.');
+  }
+};
+xhr.send();
+/*
+    xhr.open('GET', 'api/allCheckpoints', true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        
+
+        for (let i = 0; i < checkpoints.length; i++) {
+          const info = infos[i];
+          const entry = {
+            name: info.name,
+            description: info.description,
+            img: info.img,
+            audio: info.audio
+          };
+          console.log(infos);
+          const trd = entriesTable.insertRow();
+          let nameCell = trd.insertCell(0);
+          let descCell = trd.insertCell(1);
+          let imgCell = trd.insertCell(2);
+          let audioCell = trd.insertCell(3);
+
+          nameCell.innerHTML = entry.name;
+          descCell.innerHTML = entry.description;
+          imgCell.innerHTML = entry.img;
+          audioCell.innerHTML = entry.audio;
+          
+        }
+        entries.appendChild(entriesTable)
+      }
+      else {
+        console.log("No checkpoints have been created.")
+      }
+    }
+    xhr.send();
+    */
---
+++ public/server.js
@@ -0,0 +1,103 @@
+const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+const path = require('path');
+const mime = require('mime-types');
+const { pathToFileURL } = require('url');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const mimeType = mime.lookup(pathToFileURL);
+  res.setHeader('Content-Type', mimeType);
+  next();
+});
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/map.html'));
+});
+
+// Add a new checkpoint to the database
+app.post('/api/checkpoints', (req, res) => {
+  const { name, lat, lng, description, img } = req.body;
+
+  // Insert new checkpoint into the 'checkpoints' table
+  db.run(
+    `INSERT INTO checkpoints (name, lat, lng, description, img)
+          VALUES (?, ?, ?, ?, ?)`,
+    [name, lat, lng, description, img],
+    function (err) {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Internal server error.');
+      } else {
+        // Return the new checkpoint with its ID
+        const newCheckpoint = {
+          id: this.lastID,
+          name,
+          lat,
+          lng,
+          description,
+          img
+        };
+        res.json(newCheckpoint);
+      }
+    }
+  );
+});
+
+app.get('/api/checkpoints', (req, res) => {
+  db.all('SELECT * FROM checkpoints', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    } else {
+      console.log(rows);
+      res.send(rows);
+    }
+  });
+});
+
+app.get('/api/allCheckpoints', (req, res) => {
+  db.all('SELECT id,name,description,img,audio FROM checkpoints', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    } else {
+      console.log(rows);
+      res.send(rows);
+    }
+  });
+});
+
+const db = new sqlite3.Database('db/checkpoints.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the checkpoints database.');
+    // Create the 'checkpoints' table if it doesn't exist
+    db.run(`CREATE TABLE IF NOT EXISTS checkpoints (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      lat REAL NOT NULL,
+      lng REAL NOT NULL,
+      description TEXT
+    )`);
+  }
+});
+
+// Schließe die Verbindung zur Datenbank, wenn die Anwendung beendet wird
+process.on('SIGINT', () => {
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the checkpoint database connection.');
+    process.exit(0);
+  });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
